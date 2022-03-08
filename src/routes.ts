883161c18a0751572b6d01a7df63be42cd7af313
Arun Jangra <arunjngra89@gmail.com>
2022-03-08 13:42:24 +0530

    Product Routes Configured ✅

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Express, Request, Response } from "express";
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from "./controller/product.controller";
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
 import { createUserHandler } from "./controller/user.controller";
 import requireUser from "./middleware/requireUser";
 import validate from "./middleware/validateResources";
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from "./schema/product.schema";
 import { createSessionSchema } from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
+import { createProduct } from "./service/product.service";
 
 export default function (app:Express) {
     app.get('/healthcheck', (req:Request, res:Response) => {
@@ -26,4 +29,19 @@ export default function (app:Express) {
     // Logout (DELETE /api/sessions)
     app.delete("/api/sessions",requireUser, deleteSessionHandler);
     
+
+    // Product Routes
+    //---------------
+
+    // Create Product
+    app.post('/api/products', [requireUser, validate(createProductSchema)], createProductHandler);
+
+    // Update Product
+    app.put('/api/products', [requireUser, validate(updateProductSchema)], updateProductHandler);
+
+    // Get Product
+    app.get('/api/products', [requireUser, validate(getProductSchema)],getProductHandler);
+
+    // Delete Product
+    app.delete('/api/products', [requireUser, validate(deleteProductSchema)], deleteProductHandler);
 }
